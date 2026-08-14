bd3348adb317f21544a5f5e8e754cc5788ec75b2
Kevin <kazakov28082003@gmail.com>
2026-08-14 17:00:54 +0900

    feat: complated 113 lesson

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,27 +1,42 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
 
-	@Mutation(() => String)
-	public async signup(): Promise<String> {
-		console.log('Mutation:signup');
-		return this.memberService.signup();
+	@Mutation(() => Member)
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<Member> {
+		try {
+			console.log('Mutation:signup');
+			console.log('input', input);
+			return this.memberService.signup(input);
+		} catch (err) {
+			console.log('Error, signup', err);
+			throw new InternalServerErrorException(err);
+		}
+	}
+	@Mutation(() => Member)
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
+		try {
+			console.log('Mutation: login');
+			return this.memberService.login(input);
+		} catch (err) {
+			console.log('Error, login', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 	@Mutation(() => String)
-	public async login(): Promise<String> {
-		console.log('Mutation: login');
-		return this.memberService.login();
-	}
-	@Mutation(() => String)
-	public async updateMember(): Promise<String> {
+	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 	@Query(() => String)
-	public async getMember(): Promise<String> {
+	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
 	}
